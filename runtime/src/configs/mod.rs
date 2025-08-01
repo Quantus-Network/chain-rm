6d36c461770cb77252566956992284af0d988e70
illuzen <illuzen@users.noreply.github.com>
2025-08-01 12:27:27 +0800

    shortened difficulty adjustment period to reduce wobble (#144)
    Co-authored-by: illuzen <github.handheld844@passmail.net>

--- runtime/src/configs/mod.rs
+++ runtime/src/configs/mod.rs
@@ -142,7 +142,7 @@ impl pallet_qpow::Config for Runtime {
     type TargetBlockTime = TargetBlockTime;
     type AdjustmentPeriod = ConstU32<1>;
     // This is how many blocks to include for the difficulty adjustment
-    type BlockTimeHistorySize = ConstU32<25>;
+    type BlockTimeHistorySize = ConstU32<10>;
     type MaxReorgDepth = ConstU32<180>;
     type FixedU128Scale = ConstU128<1_000_000_000_000_000_000>;
     type MaxDistanceMultiplier = ConstU32<2>;
